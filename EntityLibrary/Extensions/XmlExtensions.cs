35c7ab337a1ba5dea4f096036596f7abf97e645e
Benjamin Adamson <adamson.benjamin@gmail.com>
2012-01-06 22:36:21 -0800

    Added a new project to the solution, the EntityLibrary project. Will be the project the entity system is implemented within. Nice re-usable library.

---
+++ EntityLibrary/Extensions/XmlExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System.Xml;
+using System.Xml.Linq;
+using System;
+
+namespace Smashteroids.Extensions
+{
+	public static class XmlExtensions
+	{
+		/// <summary>
+		/// Constant number of lines to skip at the top of the file, so the LineNumber 
+		/// returns the correct line-number the element is on.
+		/// </summary>
+		public const int IgnoredLines = 2;
+
+
+		/// <summary>
+		/// Return the Current line number for an xml element
+		/// </summary>
+		/// <param name="xElement"></param>
+		/// <returns></returns>
+		public static int LineNumber(this IXmlLineInfo xElement)
+		{
+			if (xElement == null)
+			{
+				throw new ArgumentNullException("xElement");
+			}
+			return xElement.LineNumber + IgnoredLines;
+		}
+	}
+}
